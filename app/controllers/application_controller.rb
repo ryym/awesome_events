049e3011da78902e694b903f53dba4e71e5f68ae
ryym <ryym.64@gmail.com>
2016-03-19 21:55:24 +0900

    app_controller: Add methods to manage log-in state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Register 'logged_in?' as a helper method to use it in views.
-  helper_method :logged_in?
+  # Register as helper methods to use them in views.
+  helper_method :logged_in?, :current_user
 
   private
+
+  # Return the current logged in user.
+  def current_user
+    return unless logged_in?
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  # Redirect to the home page when there is no logged in user.
+  # It should be used as 'before_action'.
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: 'ログインしてください'
+  end
 
   # Return whether or not the user is logged in.
   def logged_in?
