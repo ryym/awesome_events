812df19d192bba6ca8ad6e6aa8bcd85b808abbfb
ryym <ryym.64@gmail.com>
2016-03-19 17:35:11 +0900

    app_controller: Add 'logged_in?' method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Register 'logged_in?' as a helper method to use it in views.
+  helper_method :logged_in?
+
+  private
+
+  # Return whether or not the user is logged in.
+  def logged_in?
+    !! session[:user_id]
+  end
 end
