b8af43b52a93a0192760a3042b3c1b60cdac02dc
ryym <ryym.64@gmail.com>
2016-03-22 00:13:07 +0900

    Allow users to retire from this service

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  before_action :authenticate
+
+  def retire
+  end
+
+  def destroy
+    # current_user.check_all_events_finished
+    # ret = current_user.destroy
+    # binding.pry
+    # raise StandardError if ret
+    # render :retire
+    if current_user.destroy
+      reset_session
+      redirect_to root_path, notice: '退会しました'
+    else
+      render :retire
+    end
+  end
 end
