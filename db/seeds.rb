b4c9eb29da9557295d0d9cad9f2f61cd1c3f8523
ryym <ryym.64@gmail.com>
2016-03-24 23:43:40 +0900

    seed: Create many dummy events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+owner = User.first
+now = Time.zone.now
+
+99.times do |i|
+  event = Event.new(
+    name: "The event No.#{i}",
+    owner_id: owner.id,
+    start_time: now + 5.months,
+    end_time: now + 1.year,
+    place: "place-#{i}",
+    content: "This is an event ##{i}!"
+  )
+  event.save!
+end
